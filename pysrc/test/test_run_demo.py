2fa01f7c1046cdf4ef7c80da9205fe66606c89f1
YuunqiLiu <liuyuunqi@163.com>
2020-11-14 22:19:33 +0800

    change test name

--- pysrc/test/test_run_demo.py
+++ pysrc/test/test_run_demo.py
@@ -2,7 +2,7 @@ import os,sys
 import unittest
 
 # pylint: disable =unused-wildcard-import
-from ..uhdl            import *
+from ..uhdl.Demo         import *
 # pylint: enable  =unused-wildcard-import
 
 
@@ -10,7 +10,7 @@ from ..uhdl            import *
 class TestRunDemo(unittest.TestCase):
 
     def test_Crossbar(self):
-        res = Demo.Crossbar(2,2,32,1)
+        res = Crossbar(2,2,32,1)
         res.output_path = './Vout/Demo/Crossbar'
         res.generate_verilog(iteration=True)
         #print(Demo)
