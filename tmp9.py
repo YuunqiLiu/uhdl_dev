95d2f2e9e0b4514447a8e91f22d5fd59250b00aa
YuunqiLiu <liuyuunqi@163.com>
2022-09-16 20:31:54 +0800

    update lwnoc

---
+++ tmp9.py
@@ -0,0 +1,31 @@
+from pysrc.uhdl import *
+
+
+
+
+class Sub1(Component):
+
+    def __init__(self):
+        super().__init__()
+        self.out1 = Output(UInt(1))
+        self.out2 = Output(UInt(1))
+
+        self.in1 = Input(UInt(1))
+
+        self.out1 += self.in1
+        self.out2 += self.in1
+woyeb 
+class Sub2(Component):
+
+    def __init__(self):
+        super().__init__()
+        self.in1 = In
+
+
+
+
+
+test = UInt(3,1)
+
+print(test)
+print(test.father)
